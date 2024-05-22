5100b3d5895a21bd1dbea6f9096853765a6979ea
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-22 17:54:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort((a,b)=>a-b);
+	let totalCost =0;
+	while (arr.length>1){
+		let min1 = arr.shift();
+		let min2 = arr.shift();
+		let cost = min1 + min2;
+		totalCost +=cost;
+		arr.push(cost);
+		arr.sort((a,b)=>a-b);
+	}
 // return the min cost
-  
+	return totalCost;
 }
-
 module.exports=mincost;
